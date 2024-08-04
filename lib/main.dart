5ad83fed1f1e5a08a78e2cee04c8da481ed20638
evirac <ayushguptanew2@gmail.com>
2024-08-05 01:21:55 +0530

    frontend complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sound_of_meme/constants.dart';
-import 'package:sound_of_meme/pages/discover_page.dart';
-import 'package:sound_of_meme/pages/liked_songs_page.dart';
-import 'package:sound_of_meme/pages/created_songs_page.dart';
+import 'package:sound_of_meme/pages/auth_page.dart';
 
 void main() {
   runApp(SoundOfMemeApp());
@@ -21,57 +19,7 @@ class SoundOfMemeApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch()
             .copyWith(primary: kMidnightBlue, secondary: kRazerGreen),
       ),
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _selectedIndex = 0;
-  static final List<Widget> _pages = <Widget>[
-    DiscoverPage(),
-    LikedSongsPage(),
-    CreatedSongsPage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Discover',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Liked Songs',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.music_note),
-            label: 'Created Songs',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color(0xFF44D62C),
-        unselectedItemColor: Colors.white,
-        backgroundColor: const Color(0xFF001F3F),
-        onTap: _onItemTapped,
-      ),
+      home: const AuthPage(),
     );
   }
 }
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,107 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:sound_of_meme/constants.dart';
+import 'package:sound_of_meme/pages/main_page.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kMidnightBlue,
-      appBar: AppBar(
-        title: const Text(
-          'Sound of Meme',
-          style: TextStyle(color: kRazerDarkGreen),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: kMidnightBlue,
+          title:
+              const Text('Sound of Meme', style: TextStyle(color: kRazerGreen)),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Login'),
+              Tab(text: 'Register'),
+            ],
+            indicatorColor: kRazerGreen,
+            labelColor: kRazerGreen,
+            unselectedLabelColor: Colors.white,
+          ),
         ),
-        backgroundColor: kMidnightBlue,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        body: const TabBarView(
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/main');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kRazerDarkGreen,
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-              ),
-              child: const Text(
-                'Login',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/main');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kRazerDarkGreen,
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-              ),
-              child: const Text(
-                'Register',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Center(
-              child: Text(
-                'OR',
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/main');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/images/google_logo.png', height: 24),
-                    const SizedBox(width: 10),
-                    const Text(
-                      'Sign in with Google',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/main');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/images/google_logo.png', height: 24),
-                    const SizedBox(width: 10),
-                    const Text(
-                      'Sign up with Google',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            LoginTab(),
+            RegisterTab(),
           ],
         ),
       ),
@@ -114,68 +40,68 @@ class LoginTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kMidnightBlue,
-      appBar: AppBar(
-        title: const Text('Login', style: TextStyle(color: kRazerDarkGreen)),
-        backgroundColor: kMidnightBlue,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Email',
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter your email...',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Password',
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Enter your password...',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+          ),
+          const SizedBox(height: 16.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
             ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/main');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: kRazerDarkGreen,
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                ),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white),
-                ),
+          ),
+          const SizedBox(height: 16.0),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                // Handle login action
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kRazerGreen,
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(color: Colors.white),
               ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 16.0),
+          const Center(child: Text('OR')),
+          const SizedBox(height: 16.0),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                // Redirect to the main page
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              child: const Text(
+                'Sign in with Google',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -186,101 +112,87 @@ class RegisterTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kMidnightBlue,
-      appBar: AppBar(
-        title: const Text('Register', style: TextStyle(color: kRazerDarkGreen)),
-        backgroundColor: kMidnightBlue,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Name',
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter your name...',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Name',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Email',
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter your email...',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+          ),
+          const SizedBox(height: 16.0),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Password',
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Enter your password...',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+          ),
+          const SizedBox(height: 16.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Confirm Password',
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Confirm your password...',
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+          ),
+          const SizedBox(height: 16.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Confirm Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
               ),
             ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/main');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: kRazerDarkGreen,
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                ),
-                child: const Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
-                ),
+          ),
+          const SizedBox(height: 16.0),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                // Handle register action
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: kRazerGreen,
+              ),
+              child: const Text(
+                'Register',
+                style: TextStyle(color: Colors.white),
               ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 16.0),
+          const Center(child: Text('OR')),
+          const SizedBox(height: 16.0),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                // Redirect to the main page
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainPage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              child: const Text(
+                'Sign up with Google',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
